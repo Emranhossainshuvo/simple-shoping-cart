357088f07f3ca830f4fe36e97fe03fb5044548fc
emran <shuvoorba28@gmail.com>
2023-02-14 20:50:17 +0600

    first commit

---
+++ js/case.js
@@ -0,0 +1,3 @@
+document.getElementById('button-case-plus').addEventListener('click', function(){
+    console.log('there is nothing added in this cross button I am just cheacking either my code is working properly or not');
+});
